fddcd977b4ea8a4ab14bc5e106e5532f0a2ee3f0
martin <tincho8919@gmail.com>
2023-07-22 16:56:50 -0300

    otro

--- js/Formulario.js
+++ js/Formulario.js
@@ -3,9 +3,9 @@
 const nombre = document.querySelector('#nombre')
 const apellido = document.querySelector('#apellido')
 const email= document.querySelector('#email')
+const direccion = document.querySelector('#direccion')
 const edad = document.querySelector('#edad')
 const fecha = document.querySelector('#fecha')
-const direccion = document.querySelector('#direccion')
 const acuerdo= document.querySelector('#acuerdo')
 const texto  = document.querySelector('#texto ')
 const enviar = document.querySelector('#enviar')
@@ -15,9 +15,9 @@ let addDatos = () =>{
         console.log(nombre.value);
         console.log(apellido.value);
         console.log(email.value);
+        console.log(direccion.value);
         console.log(edad.value);
         console.log(fecha.value);
-        console.log(direccion.value);
         console.log(acuerdo.value);
         console.log(texto.value);
         
